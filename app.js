d0f0f2e2a379c410ca31b864fd02a030f1c86c58
koya <koya9431ptr@gmail.com>
2024-11-28 20:27:22 +0000

    done

--- app.js
+++ app.js
@@ -24,25 +24,27 @@ const cities = [
 ];
 
 // GROUP ARRAY BY STATES
-const cityGroups =
+const cityGroups = _.groupBy(cities, 'state');
 console.log(cityGroups)
 
 // SORT ARRAY ALPHABETICALLY BY CITY NAME
-const sortedCities =
+const sortedCities = _.sortBy(cities, 'city');
 console.log(sortedCities)
 
 // SHOW ONLY CITIES OF CALIFORNIA
-const californiaCities =
+const californiaCities = _.filter(cities, { state: 'California' });
 console.log(californiaCities)
 
 // SHOW ONLY CITIES OF TEXAS WITH A POPULATION OF LESS THAN 1 MILLION
-const texasCities =
+const texasCities = _.filter(cities, city => city.state === 'Texas' && city.population < 1000000);
 console.log(texasCities)
 
 // ADD ALL THE POPULATION OF CALIFORNIA CITIES
-const californiaPopulation = 
+const californiaPopulation = _.sumBy(californiaCities, 'population');
 console.log(californiaCities)
 
 // GROUP BY PARTY AND SHOW ONLY CITIES WITH A POPULATION ABOVE 1 MILLION. SORT CITY NAMES ALPHABETICALLY
-const newData =
-console.log(newData)
+const filteredCities = _.filter(cities, city => city.population > 1000000);
+const sortedCities2 = _.sortBy(filteredCities, 'city');
+const groupedByParty = _.groupBy(sortedCities2, 'party');
+console.log(groupedByParty)
